09609d9444e0b473bcd7afec1c910d067aea0729
Leticia Valladares <leticia.valladares@cloudreach.com>
2022-10-12 12:42:07 +0200

    variables updated

--- infrastructure/terraform.tfvars
+++ infrastructure/terraform.tfvars
@@ -3,11 +3,7 @@ aws_ami_owner                  = "099720109477"
 instance_type                  = "t3.medium"
 keypair                        = "talent-academy-ec2"
 aws_region                     = "eu-central-1"
-priv_subnet_a                  = "priv-subnet-1"
-priv_subnet_b                  = "priv-subnet-2"
 pub_subnet                     = "pub-subnet"
 vpc_name                       = "vpc"
-aws_pub_subnet_cidr_block_b    = "10.0.4.0/24"
-ig                             = "ig-vpc"
 
 #ssh -i ~/.ssh/talent-academy-ec2 ubuntu@3.127.98.132
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -13,21 +13,6 @@ variable "keypair" {
   type        = string
 }
 
-variable "aws_region" {
-  description = "Region to deploy"
-  type        = string
-}
-
-variable "priv_subnet_a" {
-  description = "Name of private subnet a"
-  type        = string
-}
-
-variable "priv_subnet_b" {
-  description = "Name of private subnet b"
-  type        = string
-}
-
 variable "pub_subnet" {
   description = "Name of public subnet a"
   type        = string
@@ -41,14 +26,4 @@ variable "vpc_name" {
 variable "instance_type" {
   description = "Type of aws instance"
   type        = string
-}
-
-variable "aws_pub_subnet_cidr_block_b" {
-    description = "IP range for the public subnet b"
-    type        = string
-}
-
-variable "ig" {
-    description = "Name of internet gateway"
-    type        = string
 }
